a9029c981435c6548cd24dfcf7c489a8934c7f59
Vic564 <victor.a.persson@gmail.com>
2020-04-01 19:33:42 +0200

    wildcard route 404

--- routes/errorRoute.js
+++ routes/errorRoute.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const constant = require('../constant');
+const url = require('url');
+const { ROUTE, VIEW } = require('../constant');
 
-router.get(constant.ROUTE.error, (req, res) => {
-    res.status(404).render(constant.VIEW.error, {
+router.get(ROUTE.error, (req, res) => {
+    res.status(404).render(VIEW.error, {
         errmsg: req.query.errmsg || '404. Sidan finns inte!',
         token: (req.cookies.jsonwebtoken !== undefined) ? true : false
     });
+})
+
+router.get(ROUTE.wildcard, (req, res) => {
+    res.redirect(url.format({
+        pathname: ROUTE.error,
+        query: {}
+    }));
 })
 
 module.exports = router;
