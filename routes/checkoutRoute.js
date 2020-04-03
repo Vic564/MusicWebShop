84816ced1951444541fb44c5588d81de0de2e8ff
Vic564 <victor.a.persson@gmail.com>
2020-04-03 14:39:36 +0200

    stripe betalning

--- routes/checkoutRoute.js
+++ routes/checkoutRoute.js
@@ -8,36 +8,57 @@ const stripe = require('stripe')(STRIPE.secret);
 const Order = require('../model/order');
 
 router.get(ROUTE.checkout, verifyToken, async (req, res) => {
-    if (verifyToken) {
+    try {
         const cart = await getCart(req.body.userInfo._id);
-        console.log(cart)
-        /*
-        stripe.checkout.session.create({
-            payment_method_types: ['card'],
-            list_items: showUserInfo.cart.forEach(product => {
+        const session = await stripe.checkout.sessions.create({
+            payment_method_types: ["card"],
+            line_items: cart.map(item => {
                 return {
-                    name: product.productId.name,
-                    amount: product.productId.price,
+                    name: item.product.album,
+                    amount: item.product.price * 100,
+                    quantity: item.quantity, 
+                    currency: "sek"
                 }
-            })
-        })*/
-        res.status(202).render(VIEW.checkout, {
+            }),
+            customer_email: req.body.userInfo.email,
+            success_url: req.protocol +   "://" + req.get("Host") +  ROUTE.confirmation,
+            cancel_url: req.protocol +   "://" + req.get("Host") +  ROUTE.error + '?errmsg=Stripe error'
+        });
+        const stripeVar = {
+            publicKey: STRIPE.public,
+            sessionId: session.id
+        };
+        const totalAmount = session.display_items.map(item => item.amount).reduce((acc, cur) => acc + cur) / 100;
+        return res.status(202).render(VIEW.checkout, {
             ROUTE,
             cart,
             showUserInfo: req.body.userInfo,
-            stripePublicKey: STRIPE.public,
+            stripeVar,
+            totalAmount: totalAmount,
             token: req.cookies.jsonwebtoken ? true : false
-        })
-    } else {
+        });
+    } catch (e) {
+        console.log(e)
         return res.status(202).render(VIEW.checkout, {
             ROUTE,
             showUserInfo: 'empty cart',
             token: req.cookies.jsonwebtoken ? true : false
-        })
+        });
     }
 })
 
 router.post(ROUTE.checkout, verifyToken, async (req, res) => {
+    try {
+        /* Fungerar inte...
+        await clearCart(req.body.userInfo._id);*/
+        res.redirect(ROUTE.confirmation);
+    } catch (e) {
+        console.error(e);
+        res.redirect(ROUTE.error);
+    }
+})
+
+router.get(ROUTE.confirmation, verifyToken, async (req, res) => {
     const customer = {
         fName: req.body.fName,
         lName: req.body.lName,
@@ -52,5 +73,7 @@ router.post(ROUTE.checkout, verifyToken, async (req, res) => {
 })
 
 const getCart = async (userId) => Order.find({user: userId}).populate("user product")
+
+const clearCart = async (userId) => await Order.deleteMany({user: userId})
 
 module.exports = router;
